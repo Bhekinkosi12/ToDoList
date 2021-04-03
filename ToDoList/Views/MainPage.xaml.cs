7717d6eb4c2f2d08cd6eaa312826a16164ddbc11
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-04-03 14:21:28 -0700

    Update MainPage.xaml.cs

--- ToDoList/Views/MainPage.xaml.cs
+++ ToDoList/Views/MainPage.xaml.cs
@@ -7,7 +7,7 @@ namespace ToDoList.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+    
     public partial class MainPage : TabbedPage
     {
         public MainPage()
